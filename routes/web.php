4541dd346b97d65fe3a76c531c225232634b8404
arnatronas69 <jackjefreson@gmail.com>
2024-04-27 16:02:11 +0300

    added admin authentication for the admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ThreadController;
 use App\Http\Controllers\CategoriesController;
 use App\Http\Controllers\AdminController;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
@@ -18,15 +19,17 @@ Route::get('/categories/{category}', [CategoriesController::class, 'show']);
 Route::resource('threads', ThreadController::class);
 Route::resource('threads.posts', PostController::class);
 
-Route::get('/admin', [AdminController::class, 'index'])->name('admin');
-Route::get('/admin/threads/{thread}/edit', [AdminController::class, 'editThread'])->name('admin.threads.edit');
-Route::put('/admin/threads/{thread}', [AdminController::class, 'adminUpdate'])->name('admin.threads.update');
-Route::delete('/admin/threads/{thread}', [AdminController::class, 'deleteThread'])->name('admin.threads.delete');
+Route::middleware([AdminMiddleware::class])->group(function () {
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin');
+    Route::get('/admin/threads/{thread}/edit', [AdminController::class, 'editThread'])->name('admin.threads.edit');
+    Route::put('/admin/threads/{thread}', [AdminController::class, 'adminUpdate'])->name('admin.threads.update');
+    Route::delete('/admin/threads/{thread}', [AdminController::class, 'deleteThread'])->name('admin.threads.delete');
 
-// User routes
-Route::get('/admin/users/{user}/edit', [AdminController::class, 'editUser'])->name('admin.users.edit');
-Route::put('/admin/users/{user}', [AdminController::class, 'updateUser'])->name('admin.users.update');
-Route::delete('/admin/users/{user}', [AdminController::class, 'deleteUser'])->name('admin.users.delete');
+    // User routes
+    Route::get('/admin/users/{user}/edit', [AdminController::class, 'editUser'])->name('admin.users.edit');
+    Route::put('/admin/users/{user}', [AdminController::class, 'updateUser'])->name('admin.users.update');
+    Route::delete('/admin/users/{user}', [AdminController::class, 'deleteUser'])->name('admin.users.delete');
+});
 
 Route::middleware([
     'auth:sanctum',
